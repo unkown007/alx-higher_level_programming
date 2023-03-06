d29ef0356325d38411873819ebb3332bfe5b7a63
unkown007 <victormatusse00@gmail.com>
2023-03-06 22:06:17 +0200

    create

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interative, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print("object-oriented programming with Python")
+print(str[38:66] + str[106:111] + str[0:6])
 
