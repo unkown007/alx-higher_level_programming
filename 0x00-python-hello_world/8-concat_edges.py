ba3969bbeac05ecd81c8eb6f0f867f37911c0861
unkown007 <victormatusse00@gmail.com>
2023-03-06 21:14:10 +0200

    create a new setence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interative, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[38:65]} {str[106:110]} {str:.6}")
+print(f"{str[38:66]}{str[106:111]}{str:.6}")
