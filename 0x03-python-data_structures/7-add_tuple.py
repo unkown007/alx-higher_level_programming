8153c141755f7840b4a3cd9eb8be11511abbc742
unkown007 <victormatusse00@gmail.com>
2023-03-12 20:28:53 +0200

    fixed coe

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -18,3 +18,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
                 return ((tuple_a[0] + tuple_b[0], tuple_a[1]))
             else:
                 return ((tuple_a[0] + tuple_b[0], tuple_b[1] + tuple_a[1]))
+        else:
+            return ((0, 0))
