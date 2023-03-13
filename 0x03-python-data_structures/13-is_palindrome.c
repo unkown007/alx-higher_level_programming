07b26485a17a5b76d9f1a70d3ac055e58d6e3b68
unkown007 <victormatusse00@gmail.com>
2023-03-13 15:51:34 +0200

    fixed syntax

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -30,40 +30,5 @@ int isPalindrome(listint_t **left, listint_t *right)
  */
 int is_palindrome(listint_t **head)
 {
-	/*listint_t *node;
-	int size, sum1, sum2, i;
-
-	if (head == NULL)
-		return (0);
-	if (*head == NULL)
-		return (1);
-	size = 0;
-	node = *head;
-	while (node)
-	{
-		size++;
-		node = node->next;
-	}
-	if (size == 1)
-		return (1);
-	node = *head;
-	sum1 = sum2 = 0;
-	i = 1;
-	while (node && i <= (size / 2))
-	{
-		sum1 += node->n;
-		i++;
-		node = node->next;
-	}
-	if (size % 2 != 0 && node)
-		node = node->next;
-	while (node)
-	{
-		sum2 += node->n;
-		node = node->next;
-	}
-	if (sum1 == sum2)
-		return (1);
-	return (0);*/
 	return (isPalindrome(head, *head));
 }
