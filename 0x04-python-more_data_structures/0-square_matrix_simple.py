1ec47549cddb6731c1cfd29f34c8a1a186ecb7c3
unkown007 <victormatusse00@gmail.com>
2023-03-15 19:09:31 +0200

    fixed trailing white space

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix is not None:
-        return [list(map(lambda x: x**2, elem)) for elem in matrix] 
+        return [list(map(lambda x: x**2, elem)) for elem in matrix]
