135995784aa8974d51c3d541b72b465839d0b7c8
unkown007 <victormatusse00@gmail.com>
2023-03-15 20:06:35 +0200

    added files

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        items = list(a_dictionary.items())
+        size = len(items)
+        if size > 0:
+            max_item = items[0][1]
+            for i in range(1, size):
+                if max_item < items[i][1]:
+                    max_item = items[i][1]
+                    idx = i
+            return items[idx][0]
