8f48c98d036360ed42b64c162976da8e2da3f792
unkown007 <victormatusse00@gmail.com>
2023-03-15 22:28:53 +0200

    test checker

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x**2, elem)) for elem in matrix]
+    return [list(map(lambda x: x**2, matrix[i])) elem in matrix]
