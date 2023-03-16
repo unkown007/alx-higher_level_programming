35f5fed84f17deb93b5e9db3fce17ffbdbd4eba0
unkown007 <victormatusse00@gmail.com>
2023-03-16 14:13:32 +0200

    fixed errors

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -43,38 +43,19 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t i;
-	PyObject *iter;
+	int i = 0, len = 0;
 	PyObject *item;
 
-	iter = PyObject_GetIter(p);
-	if (iter == NULL)
-	{
-		fprintf(stdout, "List is empty!\n");
-		return;
-	}
 	fprintf(stdout, "[*] Python list info\n");
-	fprintf(stdout, "[*] Size of the Python List = %ld\n", PyList_Size(p));
-	fprintf(stdout, "[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-	i = 0;
-	item = PyIter_Next(iter);
-	while (item != NULL)
+	len = PyList_GET_SIZE(p);
+	fprintf(stdout, "[*] Size of the Python List = %d\n", len);
+	fprintf(stdout, "[*] Allocated = %d\n", (int) ((PyListObject *)p)->allocated);
+	for (; i < len; ++i)
 	{
-		fprintf(stdout, "Element %ld: ", i);
-		if (PyLong_Check(item))
-			fprintf(stdout, "int\n");
-		if (PyFloat_Check(item))
-			fprintf(stdout, "float\n");
-		if (PyUnicode_Check(item))
-			fprintf(stdout, "str\n");
-		if (PyList_Check(item))
-			fprintf(stdout, "list\n");
-		if (PyTuple_Check(item))
-			fprintf(stdout, "tuple\n");
+		item = PyList_GET_ITEM(p, i);
+		fprintf(stdout, "Element %d: %s\n", i, item->ob_type->tp_name);
+
 		if (PyBytes_Check(item))
-			fprintf(stdout, "bytes\n");
-		Py_DECREF(item);
-		i++;
-		item = PyIter_Next(iter);
+			print_python_bytes(item);
 	}
 }
