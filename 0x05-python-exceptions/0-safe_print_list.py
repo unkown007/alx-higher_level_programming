30b79f53829493c5d42987cd7bcf550f9931ff0d
unkown007 <victormatusse00@gmail.com>
2023-03-27 23:07:18 +0200

    fixe error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-        except IndexError:
-            i -= 1
-            break
+    i = 0
+    if my_list is not None:
+        for i in range(0, x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+            except IndexError:
+                i -= 1
+                break
     print()
     return (i + 1)
