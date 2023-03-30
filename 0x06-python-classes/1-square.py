f6cb036d7ab61f4ac4a40efbed3cc065058f309a
unkown007 <victormatusse00@gmail.com>
2023-03-30 07:37:08 +0200

    added files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module creates a simple class that defines square
+"""
+
+
+class Square:
+    """Defines square
+    """
+    def __init__(self, size):
+        """Defines private instance attribute
+        
+        Args:
+            size(int): initialize size value
+        """
+        self.__size = size
