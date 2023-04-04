84d31726b6ad40bf83ab862274a22019c64819c1
unkown007 <victormatusse00@gmail.com>
2023-04-04 20:45:52 +0200

    added files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Adds two integer numbers
+    Args:
+        a(int): integer number
+        b(int): integer number
+"""
+def add_integer(a, b=98):
+    """  Raises:
+            TypeError: if a and is not integer or float
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
