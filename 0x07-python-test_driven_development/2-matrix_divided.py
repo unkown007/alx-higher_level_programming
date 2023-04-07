0c3aec5ba0fd4b6139ad5c33c0def1081073e94f
unkown007 <victormatusse00@gmail.com>
2023-04-08 00:28:01 +0200

    added file

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,26 +12,20 @@ def matrix_divided(matrix, div):
         or the matrix rows with different sizes or div is not a number
         ZeroDivisionError: if div is equal to 0
     """
-    if type(div) is not int and type(div) is not float:
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
-    if type(matrix) is not list or matrix == []:
-        raise TypeError("matrix must be a matrix (list of lists)\
- of integers/floats")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    size = len(matrix[0])
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists)\
- of integers/floats")
-        if size != len(row):
-            raise TypeError("Each row of the matrix must have the same size")
-        for x in row:
-            if type(x) is not int and type(x) is not float:
-                raise TypeError("matrix must be a matrix (list of lists)\
- of integers/floats")
-            new_row.append(round(x / div, 2))
-        new_matrix.append(new_row)
-    return new_matrix
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -16,6 +16,5 @@ def print_square(size):
     if size < 0:
         raise TypeError("size must be >= 0")
     for i in range(size):
-        for j in range(size):
-            print("#", end="")
+        [print("#", end="") for j in range(size)]
         print("")
