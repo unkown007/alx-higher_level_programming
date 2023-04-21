914b4534253785aa1727caec497551811a36af1b
unkown007 <victormatusse00@gmail.com>
2023-04-21 20:40:39 +0200

    added file

--- 0x0C-python-almost_a_circle/tests/rectangle_unittest.py
+++ 0x0C-python-almost_a_circle/tests/rectangle_unittest.py
@@ -9,9 +9,13 @@ class TestRectangle(unittest.TestCase):
     def test_rectangle_output(self):
         """Function to test the output the function rectangle"""
 
-        self.assertAlmostEqual(rectangle(,)
+        self.assertAlmostEqual(rectangle(,))
 
     def test_rectangle_input(self):
         """Function to test the input the function rectangle"""
 
         self.assertRaises(TypeError, rectangle, )
+
+    def test_rectangle_object(self):
+        """Fucntion to test the rectangle object creation"""
+        self.assertIs(type(rectangle(1, 2)), rectangle)
--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -3,5 +3,26 @@
 import unittest
 import json
 import sys
-import os
+from io import StringIO
 from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """
+    Class of functions to run tests
+    """
+
+    def setUp(self):
+        """
+        function to redirect stdout
+        """
+        sys.stdout = StringIO()
+
+    def tearDown(self):
+        """
+        cleans everything
+        """
+        sys.stdout = sys.__stdout__
+
+    def test_object(self):
+        self.assertIs(type(Rectangle(1, 2)), Rectangle)
