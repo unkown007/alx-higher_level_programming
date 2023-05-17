9fa7f8f0c80b417240278a2b2fb811166f8ac1b3
unkown007 <victormatusse00@gmail.com>
2023-05-17 23:17:01 +0000

    added file

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- lists all shows by their rating
+SELECT s.title, SUM(r.rate) AS rating
+FROM tv_shows AS s
+INNER JOIN tv_show_ratings AS r
+ON s.id = r.show_id
+GROUP BY s.title
+ORDER BY rating DESC;
