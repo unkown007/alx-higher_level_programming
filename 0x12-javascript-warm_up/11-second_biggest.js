05a7230c1a42c9a0acd3908ba4b26d9134e627d1
unkown007 <victormatusse00@gmail.com>
2023-06-12 21:21:02 +0200

    added file

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  let max = parseInt(process.argv[2]);
+  for (let i = 3; i < process.argv.length; i++) {
+    if (max < parseInt(process.argv[i])) {
+      max = parseInt(process.argv[i]);
+    }
+  }
+  let smax = parseInt(process.argv[2]);
+  let n = 0;
+  for (let i = 3; i < process.argv.length; i++) {
+    n = parseInt(process.argv[i]);
+    if (smax < n && n !== max) {
+      smax = n;
+    }
+  }
+  console.log(smax);
+}
