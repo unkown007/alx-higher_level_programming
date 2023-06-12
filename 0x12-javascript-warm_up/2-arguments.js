bf932fe9d49eec3f462143c1e730267d37f4c3b9
unkown007 <victormatusse00@gmail.com>
2023-06-12 16:53:25 +0200

    fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv.length > 2) {
+if (process.argv.length > 3) {
+  console.log('Arguments found');
+} else if (process.argv.length > 2) {
   console.log('Argument found');
 } else {
   console.log('No argument');
