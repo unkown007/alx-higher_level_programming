a020d03522222218cb505a9ff3c58c9465700b08
unkown007 <victormatusse00@gmail.com>
2023-06-13 16:58:47 +0200

    added file

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let str = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        str += 'X';
+      }
+      if (i < this.height - 1) {
+        str += '\n';
+      }
+    }
+    console.log(str);
+  }
+}
+
+module.exports = Rectangle;
