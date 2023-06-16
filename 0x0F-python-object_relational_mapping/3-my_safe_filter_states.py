0d81de840f2118195f44e7e60429008d7abec6ca
unkown007 <victormatusse00@gmail.com>
2023-06-16 19:39:44 +0000

    added file

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N(upper N)
+from the database hbtn_0e_0_usa"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+    cur = db.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE BINARY name = %s", (argv[4], ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
