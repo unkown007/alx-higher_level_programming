cd05f820b6a3a5a5210a5d0d81f08cb300bb4915
unkown007 <victormatusse00@gmail.com>
2023-06-17 19:47:30 +0000

    fixed

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
             pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = State(name='Lousiana')
+    state = State(name='Louisiana')
     session.add(state)
     session.commit()
     print(state.id)
