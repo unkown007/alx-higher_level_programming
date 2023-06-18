e84d634b9a85884ab669a22a97fafb208659458b
unkown007 <victormatusse00@gmail.com>
2023-06-18 22:21:09 +0000

    fixed

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""
-All states via SQLAlchemy
-"""
+""" lists all State objects, and corresponding City objects,
+contained in the database """
+
+
 from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}".format(
+                argv[1],
+                argv[2],
+                argv[3]),
+            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    session = Session(engine)
-
-    data = session.query(State).order_by(State.id).all()
-
-    for row in data:
-        print("{}: {}".format(row.id, row.name))
-        for city in row.cities:
-            print("    {}: {}".format(city.id, city.name))
-
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for s in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(s.id, s.name))
+        for city in s.cities:
+            print("\t{}: {}".format(city.id, city.name))
     session.commit()
     session.close()
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-""" Definition of a State class """
+"""
+class definition of a State and an instance Base = declarative_base()
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-
 Base = declarative_base()
 
 
 class State(Base):
-    """ State class """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    """
+    State class:
+    inherits from Base
+    links to the MySQL table states
+    class attribute id that represents a column of an auto-generated,
+    unique integer, cant be null and is a primary key
+    class attribute name that represents a column of a string
+    with maximum 128 characters and cant be null
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship(
-            'City',
-            back_populates='state',
-            cascade='all, delete, delete-orphan')
+    cities = relationship('City', cascade='save-update, merge, delete',
+                          backref='state')
