2370e7625df94e8fd579a87beb6b68edc7dbf251
unkown007 <victormatusse00@gmail.com>
2023-06-18 13:39:54 +0000

    added file

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" adds the State object 'Louisiana' to the database """
+""" changes the nae of a State object from the database """
 
 
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine(
@@ -16,8 +17,7 @@ if __name__ == "__main__":
             pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = State(name='Louisiana')
-    session.add(state)
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = 'New Mexico'
     session.commit()
     session.close()
-    print(state.id)
