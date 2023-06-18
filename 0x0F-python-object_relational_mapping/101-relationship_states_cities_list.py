5b740a2ac804f0937c953ae2c8af04a4511ffb03
unkown007 <victormatusse00@gmail.com>
2023-06-18 21:58:22 +0000

    added file

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -7,7 +7,7 @@ from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
-from sqlalchemy.orm import session
+from sqlalchemy.orm import Session
 
 
 if __name__ == "__main__":
