ca9fbfb47126c7784d44e3bfd881f3d5f5a1df06
unkown007 <victormatusse00@gmail.com>
2023-06-29 20:59:08 +0000

    fixed

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request and displays the body of the response
-curl -s -X POST -d "test@gmail.com&subject=I+will+always+be+here+for+PLD" "$1"
+curl -s -X POST -d "test@gmail.com" -d "subject=I+will+always+be+here+for+PLD" "$1"
