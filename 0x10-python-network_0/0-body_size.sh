df2126256e6a3a7abf887fa280458e850ee312d9
unkown007 <victormatusse00@gmail.com>
2023-06-29 19:40:19 +0000

    added file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,1 +1,4 @@
-curl -so /dev/null -w '%{size_download}\n' "$1"
+#!/usr/bin/env bash
+# Display the body size of HTTP response
+#curl -so /dev/null -w '%{size_download}\n' "$1"
+curl -sI "$1" | grep 'Content-Length' | sed 's/^Content-Length: //'
