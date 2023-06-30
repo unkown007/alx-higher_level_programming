b615134bcd6997eea807b20b0f6db1b4b1a0ad71
unkown007 <victormatusse00@gmail.com>
2023-06-30 11:50:35 +0000

    added file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as first argument and displays the body of the response
-curl -s -X POST -d "@$2" "$1"
+curl -s -X POST -d "@$2" "$1" -H "Content-type: application/json"
