ecbda0056e579cb98ff3d0502cf557bb4daf62a0
unkown007 <victormatusse00@gmail.com>
2023-06-30 15:07:08 +0000

    fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# fetchs https://alx-intranet.hbtn.io/status
+""" fetchs https://alx-intranet.hbtn.io/status """
 import urllib.request
 
 
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
-        print("\t- type: ", type(body))
-        print("\t- content: ", body)
-        print("\t- utf8 content: ", body.decode('utf-8'))
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
