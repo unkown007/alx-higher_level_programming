4d4a28f32bf4fe65ba1a2aa2dc0f4142b3da085c
unkown007 <victormatusse00@gmail.com>
2023-07-01 00:53:38 +0200

    fixed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,6 +8,10 @@ from requests import get
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2])
     r = get(url)
-    data = r.json()[:10]
-    for x in data:
+    data = r.json()
+    sorted_data = sorted(
+            data,
+            key=lambda d: d.get('commit').get('author').get('date'),
+            reverse=True)
+    for x in sorted_data[:10]:
         print("{}: {}".format(x['sha'], x['commit']['author']['name']))
