6456e776ea2c99f245e5e0c6128e6736448e6bb0
unkown007 <victormatusse00@gmail.com>
2023-06-30 12:38:08 +0000

    added file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message You got me!, in the body of the response
-curl -sd "You+got+me!" -X PUT "0.0.0.0:5000/catch_me" 
+curl -sd "user_id=98" -X PUT "0.0.0.0:5000/catch_me" -L
