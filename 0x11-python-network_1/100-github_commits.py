902fd66d7224b31daafbc4bb4b0807466c2c8326
unkown007 <victormatusse00@gmail.com>
2023-06-30 21:57:13 +0000

    added file

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" list 10 commits (from the recent to oldest) of the repo 'rails'
+by the user 'rails' """
+from sys import argv
+from requests import get
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2])
+    r = get(url)
+    print(r.json())
