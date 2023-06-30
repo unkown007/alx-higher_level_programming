5bfd3d06eb417f25d7b31bf573e9ae7241b231e7
unkown007 <victormatusse00@gmail.com>
2023-06-30 18:45:41 +0000

    added file

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" sends a request to the URL and displays the body of the response
+ (decoded in utf-8) """
+from sys import argv
+from urllib import request, error
+
+
+if __name__ == "__main__":
+    try:
+        response = request.urlopen(argv[1])
+        print(response.decode('utf-8'))
+    except error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
