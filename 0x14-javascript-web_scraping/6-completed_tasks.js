e0f6c75661a3e9c126b48bc0c3505ad2758580e9
unkown007 <victormatusse00@gmail.com>
2023-07-26 00:48:25 +0200

    fixed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,7 +3,7 @@
 const request = require('request');
 request(process.argv[2], (error, response, body) => {
   if (error) {
-    console.error('error: ', error);
+    console.error('error:', error);
     return;
   }
   const results = JSON.parse(body);
