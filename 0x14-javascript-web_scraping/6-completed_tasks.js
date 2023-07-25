c3c4d238d515c857ef123b5aaa1128296def5d02
unkown007 <victormatusse00@gmail.com>
2023-07-26 00:47:47 +0200

    fixed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,10 +9,13 @@ request(process.argv[2], (error, response, body) => {
   const results = JSON.parse(body);
   const tasks = {};
   for (let i = 0; i < results.length; i++) {
-    const status = results[i].completed;
     const key = results[i].userId.toString();
-    if (status) {
-      tasks[key] = tasks[key] ? tasks[key] + 1 : 1;
+    if (results[i].completed) {
+      if (tasks[key]) {
+        tasks[key]++;
+      } else {
+        tasks[key] = 1;
+      }
     }
   }
   console.log(tasks);
