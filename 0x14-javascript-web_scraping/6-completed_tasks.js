bdf0162fa8875193bd3c56100c481dc100a1bbfd
unkown007 <victormatusse00@gmail.com>
2023-07-26 22:06:08 +0200

    added file

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,21 @@
 #!/usr/bin/node
-// A script that computes the number of tasks completed by user id.
-
-const args = process.argv;
-const reqURL = args[2];
+// totals number of tasks completed per user_id, omitting those with 0
 const request = require('request');
-request(reqURL, function (error, response, body) {
+
+request(process.argv[2], function (error, response, body) {
   if (error) {
-    console.log('error:', error);
-  } else {
-    const todos = JSON.parse(body);
-    const dash = {};
-    for (let i = 0; i < todos.length; i++) {
-      const status = (todos[i].completed);
-      const key = todos[i].userId.toString();
-      if (status) {
-        if (dash[key]) {
-          dash[key]++;
-        } else {
-          dash[key] = 1;
-        }
+    console.error(error);
+  }
+  const taskList = JSON.parse(body);
+  const userTotals = {};
+  for (const task of taskList) {
+    if (task.completed) {
+      if (task.userId in userTotals) {
+        userTotals[task.userId] += 1;
+      } else {
+        userTotals[task.userId] = 1;
       }
     }
-    console.log(dash);
   }
+  console.log(userTotals);
 });
